5246fd7cf70785ba1a00ab06c776e9a0ba3dd0b3
Ollivier Robert <roberto@keltia.net>
2018-07-09 18:02:29 +0200

    More tests.

--- sandbox_test.go
+++ sandbox_test.go
@@ -14,6 +14,8 @@ func TestNew(t *testing.T) {
 	assert.NotNil(t, snd)
 	assert.Equal(t, "test", snd.tag)
 
+	defer snd.Cleanup()
+
 	tmpd := os.Getenv("TMPDIR")
 	assert.True(t, strings.HasPrefix(snd.folder, tmpd))
 
@@ -24,9 +26,25 @@ func TestNew2(t *testing.T) {
 	snd, err := New("")
 	assert.NoError(t, err)
 	assert.NotNil(t, snd)
+	snd.Cleanup()
 }
 
 func TestDir_Cwd(t *testing.T) {
+	snd, err := New("test")
+	assert.NoError(t, err)
+	assert.NotNil(t, snd)
+
+	defer snd.Cleanup()
+
+	snd.Enter()
+	spwd := snd.Cwd()
+
+	pwd, err := os.Getwd()
+	assert.NoError(t, err)
+	fpwd, err := filepath.Abs(pwd)
+	assert.NoError(t, err)
+
+	assert.Equal(t, fpwd, spwd)
 }
 
 func TestDir_Enter(t *testing.T) {
@@ -57,5 +75,33 @@ func TestDir_Enter(t *testing.T) {
 }
 
 func TestDir_Exit(t *testing.T) {
+	snd, err := New("test")
+	assert.NoError(t, err)
+	assert.NotNil(t, snd)
 
+	// Get where we are
+	opwd, err := os.Getwd()
+	assert.NoError(t, err)
+	fopwd, err := filepath.Abs(opwd)
+	assert.NoError(t, err)
+
+	snd.Enter()
+
+	// Get where we are after Enter()
+	apwd, err := os.Getwd()
+	assert.NoError(t, err)
+	fapwd, err := filepath.Abs(apwd)
+	assert.NoError(t, err)
+
+	assert.Equal(t, snd.old, fapwd)
+
+	err = snd.Exit()
+	assert.NoError(t, err)
+
+	npwd, err := os.Getwd()
+	assert.NoError(t, err)
+	fnpwd, err := filepath.Abs(npwd)
+	assert.NoError(t, err)
+
+	assert.Equal(t, fopwd, fnpwd)
 }
